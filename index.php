6d9ea6d14d0c8b773b350f9e9d9bbc101e854a88
Amy Beeston <amybeeston93@gmail.com>
2015-04-30 12:50:45 -0600

    adding index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse" style="background-color: #000022">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Amy Beeston Homepage</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Assignments</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </div>
+    <div class="container"> 
+        <h1 class="jumbotron"> Amy Beeston <br/>
+        <p>Welcome to the official home page of Amy Beeston</p> </h1>
+    </div>
+    </nav>
+    <pre>test</pre>
+</body>
+</html>
