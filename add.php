6f22c25b4ed389ad7d379b612e2aa733dfb63e17
Amy Beeston <amybeeston93@gmail.com>
2015-05-26 20:59:11 -0600

    add review form

--- add.php
+++ add.php
@@ -7,12 +7,25 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script>
+        function validate()
+        {
+            var rating = document.getElementById("rating").value;
+            if (rating > 5 || rating < 0)
+            {
+               alert("Error - The rating must be between 0 and 5");
+               document.getElementById("rating").focus();
+               return false;
+            }
+            return true;
+        }
+    </script>
     <style>
         body {
             background-color: #CCCCCC;
         }
         .col-sm-6 {
-            text-align: center;
+            text-align: left;
         }
         .jumbotron {
             background-color: #222222; 
@@ -57,7 +70,52 @@
                     <div class="jumbotron" id="col3">
                         <h2> Please fill in the following information </h2>
                     </div>
-                    
+                    <form id="form" action="browseresults.php" method="post" onsubmit="return validate()">
+                            <h4> Select a Movie </h4>
+                            <select class="form-control" name="movie" id="movie">
+<?php
+
+$dbUser = 'abeeston';
+$dbPass = 'moviepassword';
+$dbHost = '127.0.0.1';
+$dbName = 'movies';
+
+// $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+// $dbPass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+// $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+// $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+// $dbName = "movies";
+
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPass<br />\n";
+
+try
+{
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query2 = "SELECT * FROM movie";
+
+    foreach ($db->query($query2) as $row2)
+    {
+        echo '<option value="' . $row2['id'] . '">' . $row2['title'] . "</option>\n";
+    } 
+}
+catch(PDOEXCEPTION $ex)
+{
+    echo "Something bad happened: " . $ex;
+    die();
+}
+
+?>
+                            </select><br/>
+                            <label for="rating">Rating <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></label>
+                            <input type="text" class="form-control" id="rating" maxlength="1">
+                            <label for="subject">Subject</label>
+                            <input type="text" class="form-control" id="subject" maxlength="100">
+                            <label for="content">Content</label>
+                            <input type="text" class="form-control" id="content" maxlength="1000"> <br/>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                 </div>
             </div>
             <div class="col-sm-3">
