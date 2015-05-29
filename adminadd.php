c7fd7c5bcd362fb12f861b2e3e9e02e16c949efd
Amy Beeston <amybeeston93@gmail.com>
2015-05-29 10:31:10 -0600

    adding adminadd.php

---
+++ adminadd.php
@@ -0,0 +1,77 @@
+<?php
+
+    //test password
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background-color: #CCCCCC;
+        }
+        .col-sm-6 {
+            text-align: center;
+        }
+        .jumbotron {
+            background-color: #222222; 
+            color: #DDDDDD;
+        }
+        #col1 {
+            background-color: #cc3333;
+        }
+        #col2 {
+            background-color: #009933;
+        }
+        #col3 {
+            background-color: #333399;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-inverse" id="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="moviehome.php">Movie Database</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li><a href="moviehome.php">Home</a></li>
+                    <li><a href="browse.php">Browse</a></li>
+                    <li><a href="search.php">Search</a></li>
+                    <li><a href="add.php">Add a Movie</a></li>
+                    <li style="padding-left: 750px"><a href="admin.php"><span class="glyphicon glyphicon-user" class="active"></span> Admin</a></li>
+                </ul>
+            </div>
+            <div class="jumbotron">
+                <h1><br/>Admin <span class="glyphicon glyphicon-user" class="active"></span><br/></h1>
+            </div>
+        </div>
+    </nav><br/>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+                <div class="container-fluid">
+                    <div class="jumbotron" id="col3">
+                        <h2> What would you like to do? </h2>
+                    </div>
+                    <a href="addmovie.php" class="btn btn-info" role="button">Add a movie</a>
+                    <a href="addtopic.php" class="btn btn-info" role="button">Add a topic</a>
+                    <a href="removereview.php" class="btn btn-info" role="button">Remove a review</a>
+                </div>
+            </div>
+            <div class="col-sm-3">
+            </div>
+        </div>
+    </div>
+    <br/><br/>
+</body>
+</html>
